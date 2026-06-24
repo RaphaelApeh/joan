f96df8f111c69a3c5a0e52857674b5b159ca6192
raphael apeh <raphaelapeh37@gmail.com>
2026-06-24 20:58:57 +0100

    working on stuff

--- src/vm.c
+++ src/vm.c
@@ -881,7 +881,11 @@ void compile(AST* node, Chuck* chuck)
         WRITE_CHUCK(chuck, node->import_node.is_std);
         if (node->import_node.alias != NULL)
         {
-            idx = add_ident(chuck, node->import_node.alias);
+            id = add_ident(chuck, node->import_node.alias);
+            WRITE_CHUCK(chuck, OP_SET_GLOBAL);
+            WRITE_CHUCK(chuck, id);
+            WRITE_CHUCK(chuck, 0);
+            break;
         }
         WRITE_CHUCK(chuck, OP_SET_GLOBAL);
         WRITE_CHUCK(chuck, idx);
