64a9196bfa5872a9268f3f7d0e54fc2f2a31828c
Raphael apeh <132306548+RaphaelApeh@users.noreply.github.com>
2026-08-29 22:29:54 +0100

    Add str_unesc function declaration

--- include/optionals/c_string.h
+++ include/optionals/c_string.h
@@ -38,7 +38,7 @@ char* strstrp(const char* str);
 char* str_esc(const char* str);
 int strpart(const char* str, char delim, char** left, char** right);
 size_t strlen_utf8(const char* str);
-
+char* str_unesc(const char* str);
 // TODO
 bool strstrcmp(char** src, char* src2);
 
