06b3ccc5f0de3188f99d29009059608e88659b42
Raphael apeh <132306548+RaphaelApeh@users.noreply.github.com>
2026-09-20 16:46:23 +0100

    Refactor build_win function to accept compiler argument

--- nob.c
+++ nob.c
@@ -4,8 +4,10 @@
 
 #define C_FLAGS "-O2", "-Wall", "-Wextra"
 #define BUILD_FOLDER "build/"
+#define SRC_FOLDER "src/"
+#define PROJECT_LIB "libjoan"
 
-bool build_win(Nob_Cmd* cmd);
+bool build_win(Nob_Cmd* cmd, const char* cc);
 bool build_unix(Nob_Cmd* cmd);
 
 int main(int argc, char** argv)
@@ -18,17 +20,29 @@ int main(int argc, char** argv)
    Nob_Cmd cmd = {0};
    nob_cmd_append(&cmd, cc, C_FLAGS);
 #if _WIN32
-   build_win(&cmd);
+   if (!build_win(&cmd, cc)) return 1;
 #else
    build_unix(&cmd);
 #endif
-   if (!nob_cmd_run(&cmd)) return 1;
+   printf("Done\n");
    return 0;
 }
 
-bool build_win(Nob_Cmd* cmd)
+bool build_win(Nob_Cmd* cmd, const char* cc)
 {
-   NOB_TODO("NOT IMPLEMENTED build_win()\n");
+   nob_cmd_append(cmd, cc, C_FLAGS, "-DJN_BUILD_DLL", "Iinclude", "-c", SRC_FOLDER"bundle.c");
+   if (!nob_cmd_run(&cmd)) return false;
+   cmd->count = 0;
+   nob_cmd_append(cmd, cc, C_FLAGS, "-shared", "-Wl,--output-def=" BUILD_FOLDER PROJECT_LIB ".def", "-Wl,--out-implib=" BUILD_FOLDER PROJECT_LIB ".a", "-Wl,--dll", "*.o", "-o", BUILD_FOLDER PROJECT_LIB ".dll", "-static-libgcc", "-static");
+   if (!nob_cmd_run(&cmd)) return false;
+   cmd->count = 0;
+   nob_cmd_append(cmd, cc, C_FLAGS, "Iinclude", "-c", SRC_FOLDER "main.c");
+   if (!nob_cmd_run(&cmd)) return false;
+   cmd->count = 0;
+   nob_cmd_append(cmd, cc, C_FLAGS, "main.o", "-o", BUILD_FOLDER "joan.exe", "-static-libgcc", "-static", "-L" BUILD_FOLDER, "-lm", "-ljoan");
+   if (!nob_cmd_run(&cmd)) return false;
+   cmd->count = 0;
+   return true;
 }
 
 bool build_unix(Nob_Cmd* cmd)
